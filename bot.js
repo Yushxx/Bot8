5e510d0312de04a2cdf9240f465d5166c0f25c99
Yushxx <109989261+Yushxx@users.noreply.github.com>
2024-09-03 11:54:25 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,8 +102,9 @@ ${sequenceTemplateApple}
 1.23:${generateAppleSequence()}
 
 
- ♦️Attention the signal only work on \`\`\`melbet megaparis 1xbet and Linebet
-With the promo code ZFree221`\`\`\
+ ♦️Attention the signal only works on \`\`\`melbet, megaparis, 1xbet, and Linebet\`\`\` 
+with the promo code ZFree221
+
 
 [tuto en français](https://t.me/c/2035790146/9350)
 `;
